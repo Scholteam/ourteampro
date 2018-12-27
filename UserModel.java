12fbedaf002b11acb59d61dda6521887942cf6a8
Meizhizi <44289147+Meizhizi@users.noreply.github.com>
2018-12-27 23:17:41 +0800

    model层

---
+++ UserModel.java
@@ -0,0 +1,22 @@
+package com.niit.model.viewModel;
+
+public class UserModel {
+
+	private String name;
+	private String object;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getObject() {
+		return object;
+	}
+	public void setObject(String object) {
+		this.object = object;
+	}
+	
+	
+}
